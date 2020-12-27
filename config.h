b5e5bea78a9ceb100b40783d926fc0337962be44
Cristian Cazacu <vladislav.tornea@gmail.com>
2020-12-27 15:52:35 +0200

    change font size

--- config.h
+++ config.h
@@ -7,10 +7,10 @@
 /* appearance */
 static unsigned int borderpx  = 1;        /* border pixel of windows */
 static unsigned int snap      = 30;       /* snap pixel */
-static unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static unsigned int gappih    = 5;       /* horiz inner gap between windows */
+static unsigned int gappiv    = 5;       /* vert inner gap between windows */
+static unsigned int gappoh    = 5;       /* horiz outer gap between windows and screen edge */
+static unsigned int gappov    = 5;       /* vert outer gap between windows and screen edge */
 static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
@@ -19,7 +19,7 @@ static int topbar             = 1;        /* 0 means bottom bar */
  * yay -Sy libxft-bgra
  * yay -Sy ttf-joypixels
  */
-static char *fonts[]          = { "monospace:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
+static char *fonts[]          = { "monospace:size=12", "JoyPixels:pixelsize=12:antialias=true:autohint=true"  };
 
 static char normfgcolor[]           = "#F9FAF9";
 static char normbgcolor[]           = "#222B33";
